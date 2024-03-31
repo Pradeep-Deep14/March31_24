c195d19f6ef4208b012d2753368db984af077839
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-03-31 10:37:06 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+def fun():
+    for x in range(22,23,24):
+        print(x)
+fun()
+
+#range(start,stop,step)
+#Here starting value is 22.
+#Stopping value is 23.
+#Step value is 24 here it can't be done
+#Hence 22 is printed.
